7fdca955b1aabbbd66bdf0549242e1dac810c1a0
shortarrow <bamboogeneral@shortarrow.jp>
2023-03-19 22:55:39 +0900

    align center

--- src/epsodes.tsx
+++ src/epsodes.tsx
@@ -15,12 +15,20 @@ function episodeTile(props: IEpisode) {
     <Box sx={{ position: "relative" }}>
       <Paper sx={{ p: 1, backgroundColor: "#00050d" }}>
         <div>{props.parseData.watched}</div>
-        <Box sx={{ display: "flex" }}>
-          {props.parseData.episodeId}
+        <Box
+          sx={{
+            display: "flex",
+            color: "white",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Box sx={{ position: "absolute" }}>{props.parseData.episodeId}</Box>
           <CircularProgress
             variant="determinate"
             value={props.parseData.percentage}
             disableShrink
+            size={30}
             sx={{
               color: "#1a98ff",
             }}
